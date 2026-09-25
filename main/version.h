2689c7c0842a4397a987d24dace0c7c3f6147f56
Magnus Trandokken <magnus@magson.no>
2026-09-25 22:57:03 +0200

    Rename main/version.h for Ægir

--- main/version.h
+++ main/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Trimix Analyzer version information
+// Ægir version information
 #define TRIMIX_ANALYZER_VERSION_MAJOR 0
 #define TRIMIX_ANALYZER_VERSION_MINOR 2
 #define TRIMIX_ANALYZER_VERSION_PATCH 0
@@ -21,7 +21,7 @@
 
 // GitHub repository information
 #define GITHUB_OWNER "magnus188"
-#define GITHUB_REPO "ziphius"
+#define GITHUB_REPO "aegir"
 #define GITHUB_API_URL "https://api.github.com/repos/" GITHUB_OWNER "/" GITHUB_REPO "/releases/latest"
 #define GITHUB_RELEASES_URL "https://github.com/" GITHUB_OWNER "/" GITHUB_REPO "/releases"
 
